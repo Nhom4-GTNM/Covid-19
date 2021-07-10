9e0ff7c45fe3847b0aaecfc62be9036964d3dc2a
Huy_Git <hoanghuyletrang@gmail.com>
2021-07-10 12:54:15 +0700

    new-project

---
+++ main.js
@@ -0,0 +1,16 @@
+const {app,BrowserWindow} = require('electron');
+var win;
+function xuly(){
+    win = new BrowserWindow({
+        width:800,height:600,
+        
+    })
+    
+
+    win.loadFile('index.html')
+
+    
+    win.on('close',()=>{app.quit()});
+}
+
+app.on('ready',xuly);
