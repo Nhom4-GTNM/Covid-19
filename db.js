0870e07361ffc89bf6f05f1817f1ed0a8d0a5ef1
tink3t <86867851+tink3t@users.noreply.github.com>
2021-07-13 15:30:25 +0700

    Create db.js
    Update users

--- db.js
+++ db.js
@@ -13,8 +13,9 @@
 
 
 users = [
-    {phone: '0376971001', pwd: 'Tink3t' },
-    {phone: '0123456789', pwd: 'password' }
+    {phone: '0376971001', pwd: 'Tink3t', name: 'Nguyễn Văn Vinh', birth: '1990', address:' 304 Tân Kỳ Tân Qúy, phường Sơn kỳ, HCM'},
+    {phone: '0123456789', pwd: 'password',ame: 'Nguyễn Văn Không Tên', birth: '190 TCN', address:'Trà Vinh' }
+
 ]
 
 module.exports = users
