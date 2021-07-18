62d0d02e3e2645db0f4ddee15654baba815bd3a6
Luan <vanluan4440@gmail.com>
2021-07-18 15:43:02 +0700

    Update add.js

--- add_data/add.js
+++ add_data/add.js
@@ -1,6 +1,62 @@
+var fs = require("fs");
 var app = angular.module("myApp", ["ngRoute"]);
 app.controller("back", ($scope) => {
   $scope.home = function () {
     window.history.back();
+  };
+});
+app.controller("push", ($scope) => {
+  $scope.msg = "";
+  $scope.hoten = "";
+  $scope.ngaysinh = "";
+  $scope.sex = "";
+  $scope.diachi = "";
+  $scope.ngaytiem = "";
+  $scope.tenvacin = "";
+  $scope.noitiem = "";
+  $scope.tinhtrang = "";
+  var data = window.localStorage.getItem("add");
+  json = JSON.parse(data);
+  $scope.add = function () {
+    if (
+      $scope.hoten == "" ||
+      $scope.ngaysinh == "" ||
+      $scope.sex == "" ||
+      $scope.diachi == "" ||
+      $scope.ngaytiem == "" ||
+      $scope.tenvacin == "" ||
+      $scope.noitiem == "" ||
+      $scope.tinhtrang == ""
+    ) {
+      $scope.msg = " Thêm dữ liệu thất phải, vui lòng kiểm tra lại";
+    } else {
+      $scope.msg = " Dữ liệu được thêm thành công !!!";
+      (obj = {
+        id: Math.floor(Math.random() * 10000),
+        hoten: $scope.hoten,
+        ngaysinh: $scope.ngaysinh,
+        gioitinh: $scope.sex,
+        diachi: $scope.diachi,
+        tiem: [{ lan: 1, ngaytiem: $scope.ngaytiem }],
+        tenVaccine: $scope.tenvacin,
+        noitiem: $scope.noitiem,
+        tinhtrang: $scope.tinhtrang,
+      }),
+        json.push(obj);
+      const content = JSON.stringify(json);
+      fs.writeFile("./tracuu/demo.json", content, "utf8", function (err) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+      $scope.hoten = "";
+      $scope.ngaysinh = "";
+      $scope.sex = "";
+      $scope.diachi = "";
+      $scope.ngaytiem = "";
+      $scope.tenvacin = "";
+      $scope.noitiem = "";
+      $scope.tinhtrang = "";
+    }
   };
 });
